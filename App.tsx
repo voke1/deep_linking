790b0eccc5e50bfed5a2de5e0d3ccd951719e51b
Voke Olomu <vokeolomu@yahoo.com>
2023-06-20 04:50:11 +0100

    integrate deep linking

--- App.tsx
+++ App.tsx
@@ -6,113 +6,60 @@
  */
 
 import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import ScreenA from './screens/ScreenA';
+import ScreenB from './screens/screenB';
+import ScreenC from './screens/screenC';
+import LinkingScreen from './screens/LinkingScreen';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+const Stack = createNativeStackNavigator();
 
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
+function MyStack(){
+   return (
+    <Stack.Navigator>
+      <Stack.Screen name="ScreenA" component={ScreenA} />
+      <Stack.Screen name="ScreenB" component={ScreenB} />
+      <Stack.Screen name="ScreenC" component={ScreenC} />
+      <Stack.Screen name="LinkingScreen" component={LinkingScreen} />
+    </Stack.Navigator>
   );
 }
 
-function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+// const config = {
+//   screens: {
+//     screenA: 'a',
+//     screenB: 'b',
+//     screenC: 'c',
+//     }
+//   }
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+
+const config = {
+  screens: {
+    ScreenA: "a",
+    ScreenB: {
+      path: "b/:message",
+      parse: {
+        message: (message) => `${message}`
+      }
+    },
+    ScreenC: "c",
+  }
 }
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
-export default App;
+export default function App(){
+  return (
+    <NavigationContainer 
+    linking={{  
+      prefixes: ['deeplink://app'],
+      config
+    }}>
+      <MyStack/>
+    </NavigationContainer>
+  )
+};
